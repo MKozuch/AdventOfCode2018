b46706e11fda45751fc870f4f4568bb27aaf474b
MKozuch <zxdsl@student.agh.edu.pl>
2018-12-03 22:43:04 +0100

    finished day3 p1

--- Day_3/NoMatterHowYouSliceIt/NoMatterHowYouSliceIt.cpp
+++ Day_3/NoMatterHowYouSliceIt/NoMatterHowYouSliceIt.cpp
@@ -10,75 +10,93 @@
 
 #define INIT_FABRIC_SIZE 2
 
+void printFabric(const std::valarray<std::valarray<unsigned int>> &fabricGrid) {
+	if (fabricGrid.size() < 100 && fabricGrid[0].size() < 100) {
+		for (const auto &row : fabricGrid) {
+			for (const auto cell : row) {
+				if (cell)
+					printf("%d", cell);
+				else
+					printf("-");
+			}
+			printf("\n");
+		}
+	}
+}
+
 int main(int argc, char** argv)
 {
+	std::string fName;
 
-   std::string fName;
+	if (argc < 2)
+		//fName = "F:\\Code\\AdventOfCode2018\\Day_3\\test2.txt";
+	 fName = "F:\\Code\\AdventOfCode2018\\Day_3\\input.txt";
+   //fName = "C:\\Users\\MKozuch\\Desktop\\Day_3\\NoMatterHowYouSliceIt\\test.txt";
+   //fName = "C:\\Users\\MKozuch\\Desktop\\Day_3\\NoMatterHowYouSliceIt\\input.txt";
+	else
+		fName = argv[1];
 
-   if (argc < 2)
-      fName = "C:\\Users\\MKozuch\\Desktop\\Day_3\\NoMatterHowYouSliceIt\\test.txt";
-      //fName = "C:\\Users\\MKozuch\\Desktop\\Day_3\\NoMatterHowYouSliceIt\\input.txt";
-   else
-      fName = argv[1];
+	std::ifstream stream;
+	stream.open(fName);
 
-   std::ifstream stream;
-   stream.open(fName);
+	if (!stream.is_open())
+		return 1;
 
-   std::string line;
-   std::valarray<std::valarray<unsigned int>> fabricGrid(std::valarray<unsigned int>((unsigned int)0, INIT_FABRIC_SIZE), INIT_FABRIC_SIZE);
+	std::string line;
+	std::valarray<std::valarray<unsigned int>> fabricGrid(std::valarray<unsigned int>((unsigned int)0, INIT_FABRIC_SIZE), INIT_FABRIC_SIZE);
 
-   while (std::getline(stream, line)) {
+	while (std::getline(stream, line)) {
 
-      std::regex number("[[:d:]]+");
-      std::smatch match;
+		std::regex number("[[:d:]]+");
+		std::smatch match;
 
-      std::regex_search(line, match, number);
+		std::regex_search(line, match, number);
 
-      auto resBegin = std::sregex_iterator(line.begin(), line.end(), number);
-      auto resEnd = std::sregex_iterator();
-      auto matches = std::distance(resBegin, resEnd);
+		auto resBegin = std::sregex_iterator(line.begin(), line.end(), number);
+		auto resEnd = std::sregex_iterator();
+		auto matches = std::distance(resBegin, resEnd);
 
-      if (matches != 5) 
-         return 1;
+		if (line.empty())
+			break;
+		if (matches != 5)
+			return 1;
 
-      unsigned int claimNo = std::stoi((*resBegin++).str());
-      unsigned int xPos    = std::stoi((*resBegin++).str());
-      unsigned int yPos    = std::stoi((*resBegin++).str());
-      unsigned int width   = std::stoi((*resBegin++).str());
-      unsigned int length  = std::stoi((*resBegin++).str());
+		unsigned int claimNo = std::stoi((*resBegin++).str());
+		unsigned int xPos = std::stoi((*resBegin++).str());
+		unsigned int yPos = std::stoi((*resBegin++).str());
+		unsigned int width = std::stoi((*resBegin++).str());
+		unsigned int length = std::stoi((*resBegin++).str());
 
-      auto fabricLen = fabricGrid.size();
-      auto fabricWidth = fabricGrid[0].size();
+		auto fabricLen = fabricGrid.size();
+		auto fabricWidth = fabricGrid[0].size();
+		auto fabricSize = fabricLen;
 
-      while (fabricLen <= yPos + length) {
-         fabricGrid.resize(fabricLen * 2, std::valarray<unsigned int>((unsigned int)0, fabricWidth));
-         fabricLen *= 2;
-      }
+		while (fabricLen <= yPos + length || fabricWidth <= xPos + width){
+			auto copy = fabricGrid;
+			fabricGrid.resize(fabricSize * 2, std::valarray<unsigned int>((unsigned int)0, fabricSize*2));
+			
+			fabricLen = fabricGrid.size();
+			fabricWidth = fabricGrid[0].size();
+			fabricSize = fabricLen;
 
-      while (fabricWidth <= xPos + width) {
-         for (auto &row : fabricGrid) {
-            row.resize(fabricWidth * 2, 0);
+			for (int i = 0; i < copy.size(); ++i) {
+				std::copy(std::begin(copy[i]), std::end(copy[i]), std::begin(fabricGrid[i]));
+			}
          }
-         fabricWidth *= 2;
-      }
 
       for (int row = yPos; row < yPos + length; ++row) {
-         for (int col = xPos; col < xPos + length; ++col) {
-            fabricGrid[row][col]++;
+         for (int col = xPos; col < xPos + width; ++col) {
+			 (fabricGrid[row])[col] += 1;
          }
       }
    } 
 
    unsigned int count = 0;
 
+   printFabric(fabricGrid);
+
    auto fabricLen = fabricGrid.size();
    auto fabricWidth = fabricGrid[0].size();
-
-   //for (int row = 0; row < fabricLen; ++row)
-   //   for (int col = 0; col < fabricWidth; ++col)
-   //      if (fabricGrid[row][col] > 1)
-   //         ++count;
-   //printf("%d\n", count);
 
    auto count2 = std::accumulate(std::begin(fabricGrid), std::end(fabricGrid), 0,
       [](const int &sum, const std::valarray<unsigned int> &row) {
@@ -86,9 +104,3 @@ int main(int argc, char** argv)
    });
    printf("%d\n", count2);
 }
-
-
-
-// 33684 too low
-
-//126504 too high
